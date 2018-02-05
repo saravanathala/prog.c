e0b51d0ef661d79a0668c12255e4f9ffb7b69126
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-05 16:35:54 +0530

    saravana13.c

---
+++ saravana13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a,b,c,d,e;
+printf("enter the three digit number");
+scanf("%d"&a);
+if(a=e)
+printf("it ia prime");
+else
+printf("is not a prime");
+getch();
+return 0;
+}
