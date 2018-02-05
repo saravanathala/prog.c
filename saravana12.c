8cd95cdd70dc8c2dee2d143da8f20cef0d8c0fd6
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-05 16:28:57 +0530

    saravana12.c

---
+++ saravana12.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+intn,num,rev=0;
+clrscr();
+printf("enter number to check paildrome ");
+scanf("%d",&n);
+num=n;
+while(n!=0)
+{
+rev=(rev*10)+(num)
+}
+if (rev==num)
+{
+printf("enter number is a paildrome");
+}
+else
+}
+printf("enter number is not a paildrome");
+}
+getch();
+return 0;
+}
+
+
