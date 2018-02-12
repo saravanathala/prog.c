25ac14892e65cb6afbf250d0551be167563280a0
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-12 15:56:07 +0530

    saravana12 (23).c

---
+++ saravana12 (23).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int a[100],i,min=0;
+printf("enter the value of n");
+scanf("%d",\n);
+for(i=0;i<=n;i++)
+{
+printf("enter the value");
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+if(min>a[i]0;
+{
+min=a[i];
+}
+}
+printf("%d",max);
+getch ();
+return 0;
+}
