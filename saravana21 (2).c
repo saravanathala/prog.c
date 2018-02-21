0e21bdd5613ec0c49b68ff2ad22e8d2ad5969944
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-21 15:48:34 +0530

    saravana21 (2).c

---
+++ saravana21 (2).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+char str[20];
+    int length;
+    int i;
+    int count;
+    printf("Enter the string\n");
+    gets(str);
+    length=strlen(str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(!(str[i]==' '))
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+    }
+getch();
+return 0;
+}
