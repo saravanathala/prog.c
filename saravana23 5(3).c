3d3dab3d6c213b46d0a6a9d5b1dd3aa6c6c78c27
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-23 03:43:08 -0700

    saravana23 5(3).c

---
+++ saravana23 5(3).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    char a[30];
+    int i,n;
+    scanf("%s",a);
+    for(i=0;i<n;i++)
+     {
+         if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9')
+         {
+             printf("%c",a[i]);
+         }
+        getch();
+        return 0;
+        }
