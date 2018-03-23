8d33ec322c7f51c408568ebecdc4b838550d1d72
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-23 03:35:29 -0700

    saravana23 4(3).c

---
+++ saravana23 4(3).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j,temp=0;
+	char s1[100];
+	gets(s1);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		for(j=i+1;s1[j]!='\0';j++)
+		{
+			if(s1[i]>s1[j])
+			{
+				temp=s1[i];
+				s1[i]=s1[j];
+				s1[j]=temp;
+			}
+		}
+	   getch();
+     return0;
+     }
